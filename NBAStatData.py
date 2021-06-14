a7dae39d9e43f2080215dc632c4de1374d701f42
Pansiri Panaudom <biepansiri@gmail.com>
2021-06-14 14:17:12 +0700

    Update NBAStatData.py

--- NBAStatData.py
+++ NBAStatData.py
@@ -193,7 +193,7 @@ class NBAStatData:
         'NYK':'New York Knicks',
         'OKC':'Oklahoma City Thunder',
         'ORL':'Orlando Magic',
-        'PHI':'Philadelphia 76ers\\xa0(1)',
+        'PHI':'Philadelphia 76ers\xa0(1)',
         'PHO':'Phoenix Suns',
         'POR':'Portland Trail Blazers',
         'SAC':'Sacramento Kings',
